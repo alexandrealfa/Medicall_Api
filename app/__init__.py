1526ab56baae9f729cb6a2b442bbbb0b485c112e
abdielmartins <abdielmartins21@gmail.com>
2021-04-12 17:51:41 -0300

    feat: added doctors view

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,18 @@ from config import config_selector
 from app.configurations import database
 from app.configurations import serializer
 from app.configurations import migration
+from app.configurations import views
 
 
 def create_app():
     app = Flask(__name__)
+
     config_type = getenv("FLASK_ENV")
     app.config.from_object(config_selector[config_type])
+
     database.init_app(app)
     migration.init_app(app)
     serializer.init_app(app)
+    views.init_app(app)
 
     return app
--- app/configurations/views.py
+++ app/configurations/views.py
@@ -6,3 +6,9 @@ def init_app(app: Flask):
     api = Api(app)
 
     # Import views, and builds and resources
+
+    from app.views.doctor_view import Doctor, AllDoctors
+
+    api.add_resource(AllDoctors, "/api/doctors", endpoint="/api/doctors")
+    api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor", methods=["POST"])
+    api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor/<int:doctor_id>", methods=["GET", "PATCH", "DELETE"])
