8007932ebb71de917d5b0e9f6c1821f613b06712
Alexandre Alfa <Profissional@alexandrealfa.com>
2021-04-12 20:27:09 -0400

    create Patient Schema

--- app/schema/patient_schema.py
+++ app/schema/patient_schema.py
@@ -1,1 +1,18 @@
 from . import ma
+from app.models.patient_model import PatientModel
+
+
+class PatientSchema(ma.Schema):
+    class Meta:
+        model = PatientModel
+
+    id = ma.Integer()
+    firstname = ma.String()
+    lastname = ma.String()
+    phone = ma.Column()
+    email = ma.String()
+    created_at = ma.Datetime()
+
+
+patient_schema = PatientSchema()
+patients_schema = PatientSchema(many=True)
