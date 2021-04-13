f0a40e7a384f26bc142d6403aafc8c9884506b03
Matheus Beluco <math.beluco@gmail.com>
2021-04-12 22:13:56 -0300

    Episodes table created

--- app/configurations/views.py
+++ app/configurations/views.py
@@ -8,7 +8,10 @@ def init_app(app: Flask):
     # Import views, and builds and resources
 
     from app.views.doctor_view import Doctor, AllDoctors
+    from app.views.episodes_view import Episode, AllEpisodes
 
     api.add_resource(AllDoctors, "/api/doctors", endpoint="/api/doctors")
     api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor", methods=["POST"])
     api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor/<int:doctor_id>", methods=["GET", "PATCH", "DELETE"])
+    api.add_resource(AllEpisodes, "/api/episodes", endpoint="api/episodes")
+    api.add_resource(Episode, "/api/episodes", endpoint="api/episode", methods=["POST"])
