6f980701db01b0f3da7b40f1ee29595388291c30
Alexandre Alfa <Profissional@alexandrealfa.com>
2021-04-13 00:21:32 -0400

    patient view resource configurated

--- app/configurations/views.py
+++ app/configurations/views.py
@@ -6,9 +6,13 @@ def init_app(app: Flask):
     api = Api(app)
 
     # Import views, and builds and resources
-
+    from app.views.patient_view import Patients, AllPatients
     from app.views.doctor_view import Doctor, AllDoctors
 
     api.add_resource(AllDoctors, "/api/doctors", endpoint="/api/doctors")
     api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor", methods=["POST"])
     api.add_resource(Doctor, "/api/doctors", endpoint="/api/doctor/<int:doctor_id>", methods=["GET", "PATCH", "DELETE"])
+
+    api.add_resource(AllPatients, "/api/patients", endpoint="/api/patients", methods=["GET"])
+    api.add_resource(Patients, "/api/patients", endpoint="/api/patient/", methods=["POST"])
+    api.add_resource(Patients, "/api/patients", endpoint="api/patient/<int:patient_id>", methods=["GET", "PATCH", "DELETE"])
