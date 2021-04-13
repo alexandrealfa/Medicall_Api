48cb4a74089186be3b2199e9cf3cc0602a085b08
Matheus Beluco <math.beluco@gmail.com>
2021-04-12 22:48:14 -0300

    Routes under construction

--- app/models/episode_model.py
+++ app/models/episode_model.py
@@ -14,7 +14,7 @@ class EpisodeModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
-    urgency = db.Column(db.Enum(UrgencyEnum), nullable=False)
+    urgency = db.Column(db.Integer, nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey("doctors.id"), nullable=False)
     patient_id = db.Column(db.Integer, db.ForeignKey("patients.id"), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
