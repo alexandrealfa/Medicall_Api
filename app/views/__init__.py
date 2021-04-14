a32a080a3a2eb24d66069368d708ff1b8f63785f
Matheus Beluco <math.beluco@gmail.com>
2021-04-13 22:14:02 -0300

    episodes view done

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,6 +2,8 @@ from app.models.patient_model import PatientModel
 from app.schema.patient_schema import patients_schema, patient_schema
 from app.models.doctor_model import DoctorModel
 from app.schema.doctor_schema import doctor_schema, doctors_schema
+from app.models.episode_model import EpisodeModel
+from app.schema.episodes_schema import episode_schema, episodes_schema
 from flask import current_app
 
 
@@ -13,3 +15,15 @@ def db_manager(available_patient: object, deleted: bool = False):
     else:
         session.add(available_patient)
         session.commit()
+
+def is_bad_request(request):
+    is_bad = False
+
+    body_keys = [k for k in request.keys()]
+    reference = ['description', 'urgency', 'doctor_id', 'patient_id', 'created_at']
+
+    for k in body_keys:
+        if k not in reference:
+            is_bad = True
+
+    return is_bad
--- app/views/episodes_view.py
+++ app/views/episodes_view.py
@@ -1,8 +1,8 @@
-from flask_restful import Resource, reqparse, current_app
+from flask_restful import Resource, reqparse
 from http import HTTPStatus
+from flask import current_app, request
 
-from app.models.episode_model import EpisodeModel
-from app.schema.episodes_schema import episode_schema, episodes_schema
+from . import EpisodeModel, episode_schema, episodes_schema, db_manager, is_bad_request
 
 
 class AllEpisodes(Resource):
@@ -12,12 +12,33 @@ class AllEpisodes(Resource):
 
         return {"data": serializer}, HTTPStatus.OK
 
+class DoctorEpisodes(Resource):
+    def get(self, doctor_id):
+        session = current_app.db.session
+        
+        all_episodes: EpisodeModel = session.query(EpisodeModel).filter(EpisodeModel.doctor_id == doctor_id).all()
+        
+        serializer = episodes_schema.dump(all_episodes)
+
+        return {"data": serializer}, HTTPStatus.OK
+
+class PatientEpisodes(Resource):
+    def get(self, patient_id):
+        session = current_app.db.session
+        
+        all_episodes: EpisodeModel = session.query(EpisodeModel).filter(EpisodeModel.patient_id == patient_id).all()
+        
+        serializer = episodes_schema.dump(all_episodes)
+
+        return {"data": serializer}, HTTPStatus.OK
+
 
 class Episode(Resource):
     def get(self, episode_id):
-        episode = EpisodeModel.query.get(episode_id)
+        
+        episode: EpisodeModel = EpisodeModel.query.get_or_404(episode_id)
+        
         serializer = episode_schema.dump(episode)
-
         return {"data": serializer}, HTTPStatus.OK
 
     def post(self):
@@ -30,17 +51,43 @@ class Episode(Resource):
 
         kwargs = parse.parse_args()
 
-        new_episode = EpisodeModel(
-            description=kwargs.description,
-            urgency=kwargs.urgency,
-            doctor_id=kwargs.doctor_id,
-            patient_id=kwargs.patient_id,
-        )
+        new_episode = EpisodeModel(**kwargs)
 
-        session = current_app.db.session
-        session.add(new_episode)
-        session.commit()
+        db_manager(new_episode)
 
         serializer = episode_schema.dump(new_episode)
 
         return {"data": serializer}, HTTPStatus.OK
+
+
+
+    def patch(self, episode_id):
+        body = request.get_json()
+
+        parse = reqparse.RequestParser()
+        parse.add_argument("description", type=str)
+        parse.add_argument("urgency", type=int)
+        parse.add_argument("doctor_id", type=int)
+        parse.add_argument("patient_id", type=int)
+        parse.add_argument("created_at", type=str)
+
+        kwargs = parse.parse_args()
+
+        current_episode = EpisodeModel.query.get_or_404(episode_id)
+
+        if is_bad_request(body):
+                return {"msg": "bad request"}, HTTPStatus.BAD_REQUEST
+       
+        [setattr(current_episode, key, value) for key, value in kwargs.items() if value is not None]
+
+        db_manager(current_episode)
+        serializer = episode_schema.dump(current_episode)
+
+        return {"msg": "success updated","data": serializer}, HTTPStatus.OK
+
+
+    def delete(self, episode_id):
+        current_episode = EpisodeModel.query.get_or_404(episode_id)
+        db_manager(current_episode, True)
+
+        return {"msg": f"Patient {episode_id} has been deleted"}, HTTPStatus.OK
