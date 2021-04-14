b9a7a6c3d50d60513296b507094daaabdc71c00b
Matheus Beluco <math.beluco@gmail.com>
2021-04-14 19:07:49 -0300

    fixed views patch

--- app/views/patient_view.py
+++ app/views/patient_view.py
@@ -1,7 +1,8 @@
-from . import patient_schema, patients_schema, PatientModel, db_manager
+from . import patient_schema, patients_schema, PatientModel, db_manager, is_bad_request
 from flask_restful import Resource, reqparse
 from http import HTTPStatus
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask import request
 
 
 class AllPatients(Resource):
@@ -9,10 +10,7 @@ class AllPatients(Resource):
         all_patients = PatientModel.query.all()
         serializer = patients_schema.dump(all_patients)
 
-        return {
-                   "msg": "success",
-                   "data": serializer
-               }, HTTPStatus.OK
+        return {"message": "success", "data": serializer}, HTTPStatus.OK
 
 
 class Patients(Resource):
@@ -22,10 +20,7 @@ class Patients(Resource):
         current_patient = PatientModel.query.get_or_404(patient_id)
         serializer = patient_schema.dump(current_patient)
 
-        return {
-                   "msg": "success",
-                   "data": serializer
-               }, HTTPStatus.OK
+        return {"message": "success", "data": serializer}, HTTPStatus.OK
 
     def post(self):
         parse = reqparse.RequestParser()
@@ -40,13 +35,12 @@ class Patients(Resource):
         db_manager(new_patient)
         serializer = patient_schema.dump(new_patient)
 
-        return {
-                   "msg": "success created",
-                   "data": serializer
-               }, HTTPStatus.OK
+        return {"message": "success created", "data": serializer}, HTTPStatus.OK
 
     @jwt_required()
     def patch(self):
+        body = request.get_json()
+
         parse = reqparse.RequestParser()
         parse.add_argument("firstname", type=str)
         parse.add_argument("lastname", type=str)
@@ -55,21 +49,24 @@ class Patients(Resource):
         parse.add_argument("password", type=str)
 
         patient_id = get_jwt_identity()
-        print(patient_id)
         current_patient = PatientModel.query.get_or_404(patient_id)
         kwargs = parse.parse_args()
 
-        [setattr(current_patient, key, value) for key, value in kwargs.items() if key != "password" and value is not None]
+        if is_bad_request(body, kwargs.keys()):
+            return {"message": "invalid values"}, HTTPStatus.BAD_REQUEST
+
+        [
+            setattr(current_patient, key, value)
+            for key, value in kwargs.items()
+            if key != "password" and value is not None
+        ]
         if kwargs.password:
             setattr(current_patient, "password", kwargs.password)
 
         db_manager(current_patient)
         serializer = patient_schema.dump(current_patient)
 
-        return {
-                   "msg": "success updated",
-                   "data": serializer
-               }, HTTPStatus.OK
+        return {"message": "success updated", "data": serializer}, HTTPStatus.OK
 
     @jwt_required()
     def delete(self):
@@ -77,6 +74,4 @@ class Patients(Resource):
         current_patient = PatientModel.query.get_or_404(patient_id)
         db_manager(current_patient, True)
 
-        return {
-                   "msg": f"Patient {patient_id} has been deleted"
-               }, HTTPStatus.OK
+        return {"message": f"patient {patient_id} has been deleted"}, HTTPStatus.OK
