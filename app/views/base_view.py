88034c38725e60f7016f36b105cc45c7a80deb3a
Alexandre Alfa <Profissional@alexandrealfa.com>
2021-04-20 00:05:19 -0400

    feat: paginate created

--- app/views/base_view.py
+++ app/views/base_view.py
@@ -1,5 +1,5 @@
 from flask_jwt_extended import get_jwt_identity
-from flask_restful import Resource
+from flask_restful import Resource, request
 
 
 class BaseView(Resource):
@@ -12,3 +12,11 @@ class BaseView(Resource):
     def get_type(self):
         user_type = tuple(get_jwt_identity().split(" "))[1]
         return user_type
+
+    def page_pagination(self):
+        page = int(request.args.get("page")) if request.args.get("page") else 1
+        return page
+
+    def per_page_pagination(self):
+        per_page = int(request.args.get("per_page")) if request.args.get("per_page") else 15
+        return per_page
