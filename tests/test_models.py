592990146ffe1a262363bf6e75cf3b052e1b92a3
kevin wischneski <kevinwischneski@gmail.com>
2021-04-22 19:40:39 -0300

    Fix:Removing uninportable module

---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture()
+def app():
+    return create_app()
+
+
+@pytest.fixture()
+def db(app):
+    from app import database
+
+    with app.app_context():
+        database.create_all()
+        yield database
+        database.drop_all()
+        database.session.commit()
+
+
+@pytest.fixture()
+def data():
+    return {}
