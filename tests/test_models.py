e70e21679d25bad6e603521f8da00f6927acfe43
Eduardo Marinho <emartins.marinho@gmail.com>
2021-04-22 22:06:08 -0300

    alterations in tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,23 +1,50 @@
-# import pytest
-# from app import create_app
+import pytest
+from app import create_app
+from flask import current_app
+from app.models.doctor_model import DoctorModel
+from flask_sqlalchemy import SQLAlchemy
+
+import app
 
 
-# @pytest.fixture()
-# def app():
-#     return create_app()
+db = app.database
+
+@pytest.fixture()
+def app():
+    return create_app()
 
 
-# @pytest.fixture()
-# def db(app):
-#     from app import database
+@pytest.fixture()
+def db(app):
+    from app import database
 
-#     with app.app_context():
-#         database.create_all()
-#         yield database
-#         database.drop_all()
-#         database.session.commit()
+    context = app.app_context()
+    with context:
+        context.push()
+        db.create_all()
+        db.session.commit()
+        context.pop()
+
+        yield db
+        db.drop_all()
+        db.commit()
 
 
-# @pytest.fixture()
-# def data():
-#     return {}
+@pytest.fixture
+def doctor_data():
+    return {
+    "specialty": "Pediatria",
+    "id": 8,
+    "firstname": "Max",
+    "phone": "41 8888-8888",
+    "episodes_list": [],
+    "crm": "015432/PR",
+    "lastname": "Doe",
+    "email": "eae@email.com"
+  }
+
+def test_get(db, doctor_data):
+    DoctorModel.post(doctor_data)
+    
+    doctors = DoctorModel.get()
+    assert len(doctors) == 1
